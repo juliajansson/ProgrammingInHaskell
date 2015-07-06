d28ac023e1a87ba372c76a3b8fcc19ab1109b565
Patrik Jansson <patrik.ja@gmail.com>
2015-07-06 22:14:54 +0200

    Update 311.hs

--- source/311.hs
+++ source/311.hs
@@ -1,5 +1,5 @@
 second:: [a] -> a
-second xs =head (tail xs)
+second xs = head (tail xs)
 
 swap:: (a,b)->(b,a)
 swap (x,y) = (y,x)
@@ -10,10 +10,8 @@ pair x y = (x,y)
 double:: Num a => a -> a
 double x = x*2
 
-{-I need help with defining these types
-
-palindrome:: [a] -> Bool
+palindrome:: Eq a => [a] -> Bool
 palindrome xs = reverse xs == xs
 
-twice:: (a -> a )->( a -> a -> a) 
-twice f x = f (f x) -}
+twice:: (a -> a) -> a -> a
+twice   f           x  = f (f x)
