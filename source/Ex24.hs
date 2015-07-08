e02149d2e643e434c1cd98f707042fa7635a3b6d
Patrik Jansson <patrik.ja@gmail.com>
2015-07-08 19:02:28 +0200

    Some formatting + minor fixes

---
+++ source/Ex24.hs
@@ -0,0 +1,12 @@
+double :: Int -> Int
+double x = x+x
+
+quadruple :: Int -> Int
+quadruple x = double (double x)
+
+factorial :: Int -> Int
+factorial n = product [1..n]
+
+average :: [Int] -> Int
+average [] = 0
+average as = div (sum as) (length as)
