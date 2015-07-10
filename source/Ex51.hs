a4c3ebf65fc0b51768574b74527c7ed4522dc1a3
Julia Jansson <julia.jansson@gmail.com>
2015-07-10 13:47:23 +0200

    :)

---
+++ source/Ex51.hs
@@ -0,0 +1,12 @@
+--List comprehensions - generators and guards, tried out the examples
+myconcat :: [[a]] -> [a]
+myconcat xss = [x|xs <- xss, x <- xs]
+
+mylength :: [a] -> Int
+mylength xs = sum [1|_ <- xs]
+
+myfirsts :: [(a,b)] ->[a]
+myfirsts ps = [x| (x,_) <- ps]
+
+myfactors :: Int -> [Int]
+myfactors n = [x|x<-[1..n],mod n x==0]
