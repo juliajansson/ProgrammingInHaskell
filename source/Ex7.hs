9e974f2954304458d1da525927e71ae7988be623
Julia Jansson <julia.jansson@gmail.com>
2015-08-05 21:20:01 +0200

    :)

--- source/Ex7.hs
+++ source/Ex7.hs
@@ -1,7 +1,8 @@
---higher-order functions
+--7.1 higher-order functions
 quadruple::(a->a)->a->a
 quadruple f x=f(f(f(f x)))
 
+--7.2 map and filter
 map1:: (a->b)->[a]->[b]
 map1 f xs = [f x|x<-xs]
 
@@ -19,3 +20,56 @@ filter2 p (x:xs)|p x       =(x:filter2 p xs)
 
 sumsqreven:: [Int]->Int
 sumsqreven xs=sum(map (^2)(filter even xs))
+
+--7.3 foldr
+myfoldr::(a->b->b)->b->[a]->b
+myfoldr f v []    =v
+myfoldr f v (x:xs)=f x (myfoldr f v xs)
+
+{--
+sum    =foldr (+) 0
+product=foldr (*) 1
+or     =foldr (||) False
+and    =foldr (&&) True
+--}
+
+myreverse:: [a]->[a]
+myreverse xs =myfoldr klistrasist [] xs
+
+klistrasist:: a->[a]->[a]
+klistrasist x []=[x]
+klistrasist x xs=xs ++ [x]
+
+{--
+klistra::[a]->(a->[a]->[a])
+klistra ys=foldr (:) ys
+--}
+
+mylength:: [a]->Int
+mylength xs =myfoldr (\_ n -> 1+n) 0 xs
+
+{--
+mymap:: (a->b)->[a]->[b]
+mymap f xs=myfoldr op [] xs
+
+op::(a->b)->[a]->[b]
+op f (x:xs)=f x:op xs
+--}
+
+{--
+map:: (a->b)->[a]->[b]
+map f []=[]
+map f (x:xs)=(f x:map f xs)
+
+Vad är operatorn?
+formen ska bli op x (f xs)
+op f (x:xs)=f x:f xs
+--}
+
+myconcat:: [[a]]->[a]
+myconcat xs =myfoldr (++) [] xs
+
+{--
+myconcat [[81],[9,0]]=myfoldr (++) [] xs= myfoldr (++) [] (x:xs)=
+x ++ (myfoldr (++) [] xs)=[81]++[9,0]=[81,9,0]
+--}
