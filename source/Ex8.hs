750ab77d7f8fb640f46477a805133f1d0b8bbf0d
Julia Jansson <julia.jansson@gmail.com>
2015-08-22 14:47:55 +0200

    :)

--- source/Ex8.hs
+++ source/Ex8.hs
@@ -1,3 +1,4 @@
+import Data.Char (ord, chr)
 --8.2
 type Parser a = String -> [(a,String)]
 --8.3
@@ -21,4 +22,16 @@ item inp|null inp =[]
 myparse:: Parser a->String ->[(a,String)]
 myparse p inp= p inp
 
+--More parse functions
+pmap:: (a->b)->Parser a->Parser b
+pmap f pa = pb
+  where pb s = pa (f s)
+--map for parsers, not for lists
+--map:: (a->b)->[a]->[b]
 
+char2int :: Char -> Digit
+char2int c = ord c - ord '0'
+
+type Digit = Int
+digit :: Parser Digit
+digit = pmap char2int myitem
