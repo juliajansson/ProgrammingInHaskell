1e48b576c6f2f0454c58fa57383b6625e25a3d21
Julia Jansson <julia.jansson@gmail.com>
2015-09-06 17:40:48 +0200

    :)

--- source/Ex10.hs
+++ source/Ex10.hs
@@ -51,3 +51,46 @@ safediv m n =Just'(div m n)
 safehead::[a]->Maybe' a
 safehead []=Nothing'
 safehead xs=Just'(head xs)
+
+data Nat'=Zero | Succ Nat'
+--Succ (+1)
+nat2int:: Nat'->Int
+nat2int Zero    =0
+nat2int (Succ n)=1+nat2int n
+
+int2nat:: Int->Nat'
+int2nat 0 =Zero
+int2nat n =Succ (int2nat (n-1))
+
+addnat::Nat'->Nat'->Nat'
+addnat m n=int2nat (nat2int m + nat2int n)
+
+addNat:: Nat'->Nat'->Nat'
+addNat Zero n     = n
+addNat (Succ m) n =Succ (addNat m n)
+
+data List a = Nil | Cons a (List a)
+
+len::List a->Int
+len Nil        =0
+len (Cons _ xs)=1+len xs
+
+data Tree = Leaf Int | Node Tree Int Tree
+
+t:: Tree
+t = Node(Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
+
+occurs:: Int->Tree->Bool
+occurs m (Leaf n)    = m==n
+occurs m (Node l n r)= m==n || occurs m l || occurs m r
+
+flatten:: Tree->[Int]
+flatten (Leaf n)=[n]
+flatten (Node l n r)=flatten l++[n]++flatten r
+
+occurs':: Tree->[Int]
+occurs' m (Leaf n) =m==n
+occurs' m (Node l n r)
+        |m==n      =Tree
+        |m<n       =occurs' m l
+        |otherwise =occurs' m r
